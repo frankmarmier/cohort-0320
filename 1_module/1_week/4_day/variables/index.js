8da395b083dc8532ac3705703064ed91212bdfd6
yeeeehaw <proskater4553@hotmail.com>
2020-03-26 11:14:18 +0100

    var let const

---
+++ 1_module/1_week/4_day/variables/index.js
@@ -0,0 +1,32 @@
+var myVariable = "Hello world";
+
+// console.log(window.Array);
+
+for (var index = 0; index < 10; index++) {
+  // Looping...
+}
+
+console.log(index);
+
+for (var index = 0; index < 100; index++) {
+  // Looping...
+}
+
+console.log(index);
+
+for (let i = 0; i < 100; i++) {
+  // Looping...
+  //   console.log(i);
+}
+
+if (true) {
+  var myVariable = "toto";
+  let myVariable2 = "another variable";
+}
+
+console.log(otherVariable);
+
+let otherVariable = "hi";
+
+console.log(otherVariable2);
+let otherVariable2 = "hello";
