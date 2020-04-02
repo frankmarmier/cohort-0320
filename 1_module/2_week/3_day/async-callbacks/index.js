3da55d3b1ccf248a7b5c6d26bac7d82f93904d72
yeeeehaw <proskater4553@hotmail.com>
2020-04-02 07:55:44 +0200

    async / blocks

--- 1_module/2_week/3_day/async-callbacks/index.js
+++ 1_module/2_week/3_day/async-callbacks/index.js
@@ -1,17 +1,24 @@
+// Asynchronous functions :
+// setTimeout  => clearTimeout(timeoutId)
+// setInterval => clearInterval(intervalId)
+
 function foo() {
   console.log("I am foo !");
 }
 
 function goGetTheBaguetteLouisYuvalPaulineMaxime(cb) {
   let timeOutId = setTimeout(function() {
+    /* The callback function to be executed according 
+                                            to the specified timing. */
     let result = cb("We have returned with the baguettes");
     console.log(result);
     // return "We have returned with the baguettes";
     // console.log("We have returned with the baguettes");
-  }, 1000);
+  }, 5000); /* the amount of time you want to wait before executing your function 
+                specified in milliseconds  1000ms = 1s */
 
-  //   clearTimeout(timeOutId);
   //   console.log(timeOutId);
+  //    clearTimeout(timeOutId);
 }
 
 function cleanTheRoom() {
@@ -25,9 +32,15 @@ function letsStartEeating() {
 foo();
 // goGetTheBaguetteLouisYuvalPaulineMaxime(function(value) {
 //   console.log(value);
-//   letsStartEeating();
+//   letsStartEeating(); /* Since the function is executed only once the timeout is done
+//                   You are now able to eat !  */
 //   //   return "Hey";
+//   /*  Returning over here would just return a value to the scope
+//      where the function has been called. */
 // });
+
+// goGetTheBaguetteLouisYuvalPaulineMaxime(letsStartEeating);
+
 // cleanTheRoom();
 
 // let count = 0;
@@ -36,5 +49,26 @@ foo();
 //   if (count > 10) {
 //     clearInterval(intervalId);
 //   }
-//   console.log("I am called many times");
+//   console.log(count);
 // }, 1000);
+
+class MyClass {
+  constructor() {
+    this.x = 0;
+    this.intervalId = null;
+  }
+
+  startCounting() {
+    this.intervalId = window.setInterval(() => {
+      console.log(this);
+      this.x = this.x + 1;
+      clearInterval(this.intervalId);
+    }, 1000);
+  }
+}
+
+const myInstance = new MyClass();
+
+myInstance.startCounting();
+
+// console.log(myInstance)
