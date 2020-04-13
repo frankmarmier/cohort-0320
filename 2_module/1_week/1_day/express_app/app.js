29d70dea209beb8a051ca37c5352fa9fc71fdca5
yeeeehaw <proskater4553@hotmail.com>
2020-04-13 18:22:12 +0200

    express app with partials and layout

--- 2_module/1_week/1_day/express_app/app.js
+++ 2_module/1_week/1_day/express_app/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require("path");
 const server = express();
+const hbs = require("hbs");
 
 console.log(__dirname);
 
 server.use(express.static(path.join(__dirname, "public")));
 server.set("view engine", "hbs");
 
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 server.get("/", (req, res) => {
   const data = {
+    styles: ["home.css"],
     count: 42,
     title: "My First Express App",
     isLoggedIn: false,
@@ -22,7 +25,29 @@ server.get("/", (req, res) => {
     myKey: "This is my key",
   };
 
-  res.render("index", data);
+  res.render("index.hbs", data);
+});
+
+server.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    styles: ["about.css", "navbar-enhanced.css"],
+  });
+});
+
+server.get("/contact", (req, res) => {
+  res.render("contact.hbs", {
+    styles: ["contact.css", "navbar.css"],
+    users: [
+      {
+        name: "Mathias",
+        image: "/images/big_cat.jpg",
+      },
+      { name: "Olivier" },
+      { name: "Vladimur" },
+      { name: "Louis" },
+    ],
+    // scripts: ["toto.js"]
+  });
 });
 
 server.listen(3000, () => {
