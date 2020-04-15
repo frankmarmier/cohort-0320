064d43b68058711bf6e0e50ed76c8e3c8d30bc4e
yeeeehaw <proskater4553@hotmail.com>
2020-04-15 17:46:07 +0200

    comments

--- 2_module/1_week/3_day/express_app/routes/recipes.js
+++ 2_module/1_week/3_day/express_app/routes/recipes.js
@@ -4,7 +4,14 @@ const recipesModel = require("../models/recipes.js");
 
 // /recipes/
 // url http://localhost:4000/recipes/
+
+// You can pass a request parameter to all routes by sending a request to the endpoint/route
+// followed by a question mark and a set of key value pairs seperated with and ampersand "&"
+//  e.g   http://localhost:4000/recipes?cuisine=Italian&creator=Toto
+//   The request query will be accessible at req.query and the result of the query displayed above will be
+//    an object =>  {cuisine: "Italian", creator:"Toto"}
 router.get("/recipes", (req, res) => {
+  console.log(req.query);
   recipesModel
     .find({})
     .then((dbResult) => {
@@ -18,8 +25,10 @@ router.get("/recipes", (req, res) => {
     });
 });
 
-// /recipes/:id
 router.get("/recipes/:id", (req, res) => {
+  //                  ^
+  //                  |
+  // Request parameter accessible through req.params.id
   recipesModel
     .findById(req.params.id)
     .then((dbResult) => {
