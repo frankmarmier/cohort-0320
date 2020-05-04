05231a4c78cf7fdcfa40a09d921ff6506dd71499
yeeeehaw <frankomarmier@gmail.com>
2020-05-04 12:38:29 +0200

    higher order functions review

---
+++ 3_module/1_day/1_HOF_Higher_Order_Functions/index.js
@@ -0,0 +1,76 @@
+/** Let's review !
+ *  HIGHER ORDER FUNCTIONS !
+ */
+
+const aliments = [
+  { name: "Banana", type: "fruit", weight: 100, icon: "🍌" },
+  { name: "Raisin", type: "fruit", weight: 2, icon: "🍇" },
+  { name: "Apple", type: "fruit", weight: 110, icon: "🍏" },
+  { name: "Cucumber", type: "vegetable", weight: 130, icon: "🥒" },
+  { name: "Eggplant", type: "vegetable", weight: 130, icon: "🍆" },
+  { name: "Spinach", type: "vegetable", weight: 70, icon: "🥬" },
+  { name: "Potatoe", type: "vegetable", weight: 500, icon: "🥔" },
+  { name: "Melon", type: "fruit", weight: 4000, icon: "🍉" },
+  { name: "Kiwi", type: "fruit", weight: 30, icon: "🥝" },
+];
+
+console.log("****** Iteration 1 *******");
+console.log("********************************");
+/* Loop through the aliments and print their names
+ * Method: forEach()
+ *
+ */
+
+// Write here
+console.log("****** End of iteration 1 *******");
+console.log("*********************************");
+
+console.log("****** Iteration 2 *******");
+console.log("********************************");
+/* Filter the aliments =>  Create 2 arrays !
+ * 1 array containing all the fruits.
+ * 1 array containing all the vegetables.
+ * Method: filter()
+ */
+
+// Write here
+
+console.log("****** End of iteration 2 *******");
+console.log("*********************************");
+
+console.log("****** Iteration 3 *******");
+console.log("********************************");
+/*  Map both the fruits & vegetables array => Create 2 new arrays.
+ * 1 array containg all the fruits icons.
+ * 1 array containg all the vegetables icons.
+ * Method: map()
+ */
+
+// Write here
+
+console.log("****** End of iteration 3 *******");
+console.log("*********************************");
+
+console.log("****** Iteration 4 *******");
+/* Using the 2 arrays you've created in iteration 2.
+ * Make a sum of the fruits and then the vegetables.
+ * Method: reduce()
+ * Which way weighs more ?
+ */
+
+// Write here
+
+console.log("****** End of iteration 4 *******");
+console.log("*********************************");
+
+console.log("****** Iteration 5 *******");
+console.log("*********************************");
+/* Chaining.
+ * 1 - From the aliments array, chain a filter and a reduce
+ * to calculate the total weight of the fruits.
+ * 2 - From the aliments array, chain a filter and a reduce
+ * to calculate the total weight of the vegetables.
+ */
+
+console.log("****** End iteration 5 *******");
+console.log("*********************************");
