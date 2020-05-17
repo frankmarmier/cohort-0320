07229639aea8d9c6604e2b766f21382f7e89d5d6
yeeeehaw <frankomarmier@gmail.com>
2020-05-17 22:41:39 +0200

    Button.jsx & Button.scss

--- captain_hooks/src/Components/Button.jsx
+++ captain_hooks/src/Components/Button.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../styles/Button.scss";
 
-const Button = ({ sign, handleClick, className, isFontAwesome, children }) => {
+const Button = ({
+  type = "default",
+  className = "",
+  isFontAwesome,
+  children,
+  handleClick,
+}) => {
   if (!isFontAwesome) {
     return (
-      <button className={"btn " + className} onClick={handleClick}>
+      <button
+        className={`Button Button--${type} ${className} `}
+        onClick={handleClick}
+      >
         {children}
       </button>
     );
   }
-
-  return (
-    <button onClick={handleClick} className={className}>
-      <FontAwesomeIcon icon={sign} />
-    </button>
-  );
 };
 
 export default Button;
