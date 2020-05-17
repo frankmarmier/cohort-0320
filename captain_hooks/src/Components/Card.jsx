cdf48e7664b0b0b069672277fcc44e5eba046b9a
yeeeehaw <frankomarmier@gmail.com>
2020-05-17 21:46:48 +0200

    Removed element from data.json

---
+++ captain_hooks/src/Components/Card.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/Card.scss";
+
+export const Title = ({ children }) => {
+  return <h2 className="Card__title">{children}</h2>;
+};
+
+export const SubTitle = ({ children }) => {
+  return <h3 className="Card__subtitle">{children}</h3>;
+};
+
+export const Image = ({ url, alt }) => {
+  return <img className="Card__image" src={url} alt={alt} />;
+};
+
+export const Content = ({ children }) => {
+  return <p className="Card__content">{children}</p>;
+};
+
+export const Link = ({ children, url }) => {
+  return (
+    <a href={url} rel="noopener noreferrer" target="_blank">
+      {children}
+    </a>
+  );
+};
+
+const Card = ({ children }) => {
+  return <aside className="Card">{children}</aside>;
+};
+
+export default Card;
