0b0f76dfaa6d276a79481ea2c7ffce50372d5c80
yeeeehaw <frankomarmier@gmail.com>
2020-05-17 21:47:30 +0200

    Container.jsx & Container.scss

---
+++ captain_hooks/src/Components/Container.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Container.scss";
+
+const Container = ({ children }) => {
+  return <div className="Container">{children}</div>;
+};
+
+export default Container;
