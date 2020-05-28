74e9c32a87349a85e4cef46a883412fdee638951
yeeeehaw <frankomarmier@gmail.com>
2020-05-28 10:02:03 +0200

    Add data to data.json

--- captain_hooks/src/Components/Hooks/Dependencies.jsx
+++ captain_hooks/src/Components/Hooks/Dependencies.jsx
@@ -1,13 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Container from "Components/Container";
 import Button from "Components/Button";
 
 const Dependencies = (props) => {
   const [value, setValue] = useState(0);
+  const [timer, setTimer] = useState(0);
 
   const increment = (event) => {
     setValue((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    console.log("In useEffect !");
+    // Called first time after first render, then every time component re-renders
+    // no matter if props or state changed.
+  }); // No dependency, called every render after first render
+
+  useEffect(() => {
+    // "componentDidMount" and "componentWillUnMount"
+    console.log(
+      "I am called once and then never again because my dependencies array is empty !"
+    );
+
+    return () => {
+      // componentWillUnMount
+      console.log(
+        "I am registered once on mount, and whenever the component unmounts, i will be called"
+      );
+    };
+  }, []); // componentDidMount
+
+  useEffect(() => {
+    console.log("I am called first then when only props change", props);
+  }, [props]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setTimer(timer + 1);
+    }, 1000);
+  }, []);
 
   return (
     <Container>
@@ -15,7 +46,7 @@ const Dependencies = (props) => {
       <Button handleClick={increment} type="primary">
         {value}
       </Button>
-      <h1>Interval value: {value}</h1>
+      <h1>Timer value: {timer}</h1>
     </Container>
   );
 };
