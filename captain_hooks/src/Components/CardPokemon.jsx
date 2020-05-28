a28aa928e474be8cb0dc034bb2acf69f82ce35fd
yeeeehaw <frankomarmier@gmail.com>
2020-05-28 09:03:14 +0200

    Change CardPokemon layout

--- captain_hooks/src/Components/CardPokemon.jsx
+++ captain_hooks/src/Components/CardPokemon.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import "styles/CardPokemon.scss";
 
 const CardPokemon = ({ pokemon }) => {
+  if (!pokemon) return null;
+
+  const { base } = pokemon;
+
+  const stats = Object.keys(base).map((key) => (
+    <li key={key}>
+      {key}: {base[key]}
+    </li>
+  ));
+
   return (
-    <div className="CardPokemon">
-      <pre>{JSON.stringify(pokemon, null, 2)}</pre>
+    <div className="CardPokemon text-align-center">
+      <img src={pokemon.picture} alt={pokemon.ename} />
+      <h4 style={{ fontSize: "2rem" }}>
+        {pokemon.ename} |{" "}
+        <span className="CardPokemon__jname">{pokemon.jname}</span>
+      </h4>
+      <ul className="CardPokemon__stats">{stats}</ul>
     </div>
   );
 };
