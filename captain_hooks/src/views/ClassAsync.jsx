e2dc972ffa6726f159f74f608b62a0e63eb69056
yeeeehaw <frankomarmier@gmail.com>
2020-05-28 09:02:11 +0200

    Change state from url to id

--- captain_hooks/src/views/ClassAsync.jsx
+++ captain_hooks/src/views/ClassAsync.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
-import ClassListPokemon from "Components/ClassListPokemon";
-import ClassPokemon from "Components/ClassPokemon";
+import ClassListPokemon from "Components/Class/ClassListPokemon";
+import ClassPokemon from "Components/Class/ClassPokemon";
 import Container from "Components/Container";
+import "styles/HookAsync.scss";
 
 class UsingAsync extends Component {
   state = {
-    selectedPokemon: "",
+    selectedPokemon: null,
   };
 
-  handleSelectedPokemon = (index) => {
-    this.setState({ selectedPokemon: index + 1 });
+  handleSelectedPokemon = (id) => {
+    this.setState({ selectedPokemon: id });
   };
 
   render() {
     return (
       <Container>
-        <h1 className="title">Lets get some Pokemons !</h1>
-        <div className="d-flex">
+        <h1 className="title text-align-center">Lets get some Pokemons !</h1>
+        <div className="Pokemon-grid">
           <ClassListPokemon pokemonHandler={this.handleSelectedPokemon} />
-          {this.state.selectedPokemon && (
+          {this.state.selectedPokemon !== null && (
             <ClassPokemon selectedPokemon={this.state.selectedPokemon} />
           )}
         </div>
